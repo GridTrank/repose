f347c7784fe8cc84bcfde4eefc945f073370bca2
lk543061231 <543061231@qq.com>
2021-08-26 21:38:27 +0800

    111

--- src/components/common/commonQuote.vue
+++ src/components/common/commonQuote.vue
@@ -180,7 +180,7 @@ export default {
         // 选择文章
         selectArticle(val){
             if(val.title || val.subject){
-                this.setTitle=val.title.substring(25,0) ||  val.subject.substring(25,0)
+                this.setTitle=val.title.substring(25,3)+'...' ||  val.subject.substring(25,3)+"..."
             }
             this.childItem=val
             if(val){
